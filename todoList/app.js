66a79361615c9431fda7ed7fb7011c325aefc6e1
merrifieldneal <merrifield.neal@live.com>
2022-12-10 01:09:56 -0500

    Add files via upload

--- todoList/app.js
+++ todoList/app.js
@@ -3,59 +3,63 @@ const submitTaskButton = document.body.querySelector('form button');
 const submitField = document.body.querySelector('form input');
 const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
 //console.log(savedTodos);
+
 //create variable from existing localStorage
 for (let i = 0; i < savedTodos.length; i++) {
     // iterate through each saved li entry
     let newTodo = document.createElement('li');
     newTodo.innerText = savedTodos[i].task;
-    newTodo.isCompleted = savedTodos[i].iscompleted ? true : false;
+    newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
     if (newTodo.isCompleted) {
-        newTodo.style.textDecoration = 'line-through';
+        newTodo.className = 'line';
     }
     const newButton = document.createElement("button");
     newButton.innerText = "X";
     newTodo.append(newButton);
     todoList.appendChild(newTodo);
 }
-// remove task 
+
+// remove task linethrough
 todoList.addEventListener('click', function (e) {
-    const help = e.target.parentElement;
+    const { help } = JSON.parse(localStorage.getItem('todos'));
+    //console.log(help);
+    //let clickedListItem = e.target;
     if (e.target.tagName === 'BUTTON') {
         e.target.parentElement.remove();
-
-        localStorage.removeItem("todos");
         for (let i = 0; i < savedTodos.length; i++) {
-            if (savedTodos[i] === e.target.innerText) {
-                savedTodos.remove();
+            let taskX = (e.target.parentElement.innerText).slice(0, (e.target.parentElement.innerText.length - 1));
+            // focus on what data we are comparing here.
+            if (savedTodos[i].task === taskX) {
+                savedTodos.splice([i], 1);
             }
         }
-        console.log(savedTodos);
-        savedTodos = '';
         localStorage.setItem("todos", JSON.stringify(savedTodos));
         //delete savedTodos[0];
     }
+    if (e.target.tagName === 'LI') {
+        if (!e.target.isCompleted) {
+            e.target.style.textDecoration = "line-through";
+            e.target.isCompleted = true;
+        } else {
+            e.target.style.textDecoration = "none";
+            e.target.isCompleted = false;
+        }
+
+        //breaks for duplicates - another option is to have dynamic IDs
+        for (let i = 0; i < savedTodos.length; i++) {
+            let taskX = (e.target.innerText).slice(0, (e.target.innerText.length - 1));
+            if (savedTodos[i].task === taskX) {
+                savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
+                localStorage.removeItem('todos');
+                localStorage.setItem("todos", JSON.stringify(savedTodos));
+
+            }
+
+        }
+
+    }
 })
-// seperate listener for line through 
-// todoList.addEventListener("click", function (event) {
-//     let clickedListItem = event.target;
-//     console.log('you clicked it');
 
-//     if (!clickedListItem.isCompleted) {
-//         clickedListItem.style.textDecoration = "line-through";
-//         clickedListItem.isCompleted = true;
-//     } else {
-//         clickedListItem.style.textDecoration = "none";
-//         clickedListItem.isCompleted = false;
-//     }
-
-//     breaks for duplicates - another option is to have dynamic IDs
-//     for (let i = 0; i < savedTodos.length; i++) {
-//         if (savedTodos[i].task === clickedListItem.innerText) {
-//             savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
-//             localStorage.setItem("todos", JSON.stringify(savedTodos));
-//         }
-//     }
-// });
 // add task
 submitTaskButton.addEventListener('click', function (e) {
     e.preventDefault();
